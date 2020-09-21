5a40b9c4fecdba7d8a69f07e6b114c3a9ff4de7f
Brad Larson <larsonbrad@pm.me>
2020-09-21 16:20:33 -0400

    Created some files and named stuff

---
+++ frontend/webfilms.jsx
@@ -0,0 +1,7 @@
+import react from "react";
+import reactDOM from "react-dom";
+
+document.addEventListener("DOMContentLoaded", () => {
+    const root = document.getElementById('root')
+    ReactDOM.render(<h1>Webfilms</h1>, root)
+})
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     context: __dirname,
-    entry: "./frontend/index.jsx",
+    entry: "./frontend/webfilms.jsx",
     output: {
         path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
         filename: "bundle.js"
