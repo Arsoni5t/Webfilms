38d1f6fb7e8da575af681d811ff2204ef9549d38
Brad Larson <larsonbrad@pm.me>
2020-09-22 10:34:21 -0400

    Routes setup

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,11 +1,25 @@
 class Api::SessionsController < ApplicationController
 
-    def new 
-        /create??/
+    def create
+        @user = User.find_by_credentials(params[:user][:email], params[:user][:password])
 
+        if @user
+            login(@user)
+            render "api/users/show"
+        else
+            render json: ["Something's very wrong"], status: 401
+        end
     end
 
     def destroy
+        @user = current_user
+
+        if @user
+            logout
+            render "api/users/show"
+        else
+            render json: ["Knock Knock? Who's There? --- No one"], status: 404
+        end
 
     end
 
--- app/controllers/api/user_controller.rb
+++ app/controllers/api/user_controller.rb
@@ -1,6 +1,14 @@
 class Api::UsersController < ApplicationController
     def create
+    
+        @user = User.new(user_params)
 
+        if @user.save
+            login(@user)
+            render "api/users/show"
+        else 
+            render json: @user.errors.full_messages, status: 422
+        end
     end
 
     def index
@@ -13,5 +21,11 @@ class Api::UsersController < ApplicationController
 
     def destroy
 
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:email, :password)
     end
 end
