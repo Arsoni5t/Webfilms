dfe3f570e40b28c11a2a12dabe5f47682c8c7348
Brad Larson <larsonbrad@pm.me>
2020-09-22 12:13:03 -0400

    api utils

--- frontend/webfilms.jsx
+++ frontend/webfilms.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { login, logout } from "./util/session_api_util";
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById('root')
+    window.login = login
+    window.logout = logout 
     ReactDOM.render(<h1>Webfilms - Coming This Fall</h1>, root)
 })
