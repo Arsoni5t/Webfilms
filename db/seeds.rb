5b6948eb02045e6044b97353865fe441bb4e2732
Brad Larson <larsonbrad@pm.me>
2020-09-29 13:53:23 -0400

    Updated seed minor change

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-require 'open-uri' #not sure if I need this
+# require 'open-uri' #not sure if I need this
 
 User.destroy_all
 Film.destroy_all
