cc16c20f46254b91c2b6d041b3cf17e4f8974450
Brad Larson <larsonbrad@pm.me>
2020-11-11 15:34:44 -0500

    sep nav component

--- frontend/components/modal/modal.jsx
+++ frontend/components/modal/modal.jsx
@@ -2,21 +2,13 @@ import React from 'react';
 import { closeModal } from '../../actions/modal_actions';
 import { connect } from 'react-redux';
 import FilmShow from '../film/film_show'
-// import LoginFormContainer from '../session_form/login_form_container';
-// import SignupFormContainer from '../session_form/signup_form_container';
+
 
 function Modal({ modal, closeModal, films }) {
     if (!modal) {
         return null;
     }
-    // let component;
-    // switch (modal) {
-    //     case 'show':
-    //         component = <FilmShow />;
-    //         break;
-    //     default:
-    //         return null;
-    // }
+
     const selectedFilm = films[modal]
     return (
         <div className="modal-background" onClick={closeModal}>
